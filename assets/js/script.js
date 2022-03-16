e95b4117751e6fce7c63f998300f770832dce877
Joseph Anderson <jandrewanderson1995@gmail.com>
2022-03-16 15:53:26 -0500

    attempted to create some functions in my TODO

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,13 @@
+//global variables
+var searchInput;
+var lon;
+var lat;
+//var for the search input
+var search = $('#search');
+//var for search button
+var searchBtn = $('#search-btn')
+
+
 //this is the current day. added to the correct area of the html
 $('#date').html(moment().format('ddd MM/DD/y'))
 
@@ -11,12 +21,76 @@ function fiveDays(){
 }
 fiveDays();
 
+
+
+//functions that will gather data from the api
+
+    //function to gather data from current weather API
+function requestCurrent(){
+
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + searchInput +'&units=imperial&APPID=65293d15fd1af0117bccda69dffb91bb'
+
+
+    fetch(requestUrl)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        // var memesArray = data.data.memes;
+        // console.log(memesArray)
+
+        // memesArray.forEach(item => {
+        //     var url = item.url
+
+        //     var img = $(`<img src=${url} alt="meme">`);
+        //     main.append(img);
+        // })
+    })
+
+}
+
+    //function to gather data from the future forecast API
+function requestFiveDays(){
+
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/onecall?' + lat +'&' + lon + '&units=imperial&exclude=current,minutley,hourly,alerts&APPID=65293d15fd1af0117bccda69dffb91bb'
+
+
+    fetch(requestUrl)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        // var memesArray = data.data.memes;
+        // console.log(memesArray)
+
+        // memesArray.forEach(item => {
+        //     var url = item.url
+
+        //     var img = $(`<img src=${url} alt="meme">`);
+        //     main.append(img);
+        // })
+    })
+
+}
+
+
 //TODO: 
 
-    //create a function that will gather data from the api
-    
     //create a function that will capture the value of the search bar and add it to the api address
         //save these searches to local storage. 
+
+function searchBar (event){
+    event.preventDefault();
+    var searchValue = search.val()
+    console.log(searchValue);
+    localStorage.setItem('searchValue', searchValue);
+}
+
+var mySearchHist = localStorage.getItem('searchValue');
+
+        //add event listener to the button
+searchBtn.on('submit' , searchBar);
+
 
     //create a function that will dynamically add a button of whatever city was searched in the input (search bar)
         //create another function that will capture saves searches from local storage and create a button for the cities that were stored there.
